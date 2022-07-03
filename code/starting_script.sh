2a78dbedc6991a71c0fba02049042693d2d95a2b
Prakhar Khandelwal <prakharcs01@gmail.com>
2022-07-03 19:16:58 +0530

    bux fixed

--- code/starting_script.sh
+++ code/starting_script.sh
@@ -3,10 +3,10 @@
 if [[ !  -d  "/root/.ssh" ]]; then
 mkdir /root/.ssh/ ;
 if [[ ! -z "${public_key}" ]]; then
-echo $public_key > /root/.ssh/authorized_keys
+echo $public_key >> /root/.ssh/authorized_keys
 else 
 read public_key ;
-echo $public_key > /root/.ssh/authorized_keys
+echo $public_key >> /root/.ssh/authorized_keys
 fi
 fi
 
