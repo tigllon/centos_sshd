0d9812a000bc04bdd60b19ca18d314c83d7de857
Prakhar Khandelwal <prakharcs01@gmail.com>
2022-07-03 20:34:59 +0530

    bug fixed

--- code/starting_script.sh
+++ code/starting_script.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/bash
-
 if [[ !  -d  "/root/.ssh" ]]; then
-mkdir /root/.ssh/ ;
+     mkdir /root/.ssh/ ;
 if [[ ! -z "${public_key}" ]]; then
-echo $public_key >> /root/.ssh/authorized_keys
-else 
-read public_key ;
-echo $public_key >> /root/.ssh/authorized_keys
+     echo $public_key >> /root/.ssh/authorized_keys;
 fi
 fi
 
