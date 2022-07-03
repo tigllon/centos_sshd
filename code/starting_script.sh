f9990a567ea152ec63d0033a193ba24da9c698b7
Prakhar Khandelwal <prakharcs01@gmail.com>
2022-07-03 21:00:59 +0530

    bug fixed

--- code/starting_script.sh
+++ code/starting_script.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/bash
+# Check if directory is available or not
 if [[ !  -d  "/root/.ssh" ]]; then
      mkdir /root/.ssh/ ;
+# Check if environment variables are set or not
 if [[ ! -z "${public_key}" ]]; then
      echo $public_key >> /root/.ssh/authorized_keys;
+else 
+     echo "OOPS!! You  have to pass the public key too ( See README )";
+     exit;
 fi
 fi
 
